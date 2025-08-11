1dbadaf6c94a193dafa6736eb0953a3bd6ba65e1
Right Fix <righteousnessude@gmail.com>
2025-08-11 14:39:04 +0100

    update man.py

--- man.py
+++ man.py
@@ -14,7 +14,7 @@
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split as split
 from sklearn.linear_model import LinearRegression
