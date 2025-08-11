ac24e84f0093f36f1cbd863eef6d0ff7536dfe04
Right Fix <righteousnessude@gmail.com>
2025-08-11 17:57:05 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,17 @@ st.write(
     "Select your laptop specification So you would know your budget"
 )
 
-company = st.selectbox( "Company" , sorted(list(svb["company"])), index = "", placeholder = "Company")
-product = st.selectbox( "Product" , sorted(list(svb["product"])), index = "", placeholder = "Product")
-typename = st.selectbox( "Type Name", sorted(list(svb["typename"])), index = "", placeholder = "Type Name")
-inches = st.selectbox( "Inches", sorted(list(svb["inches"])), index = "", placeholder = "Inches")
-screenresolution =  st.selectbox( "Screen Resolution" , sorted(list(svb["screenresolution"])), index = "", placeholder = "Screen Resolution")
-cpu= st.selectbox( "CPU", sorted(list(svb["cpu"])), index = "", placeholder = "CPU")
-ram = st.selectbox( "RAM", sorted(list(svb["ram"])), index = "", placeholder = "RAM")
-memory = st.selectbox( "Memory", sorted(list(svb["memory"])), index = "", placeholder = "Memory",)
-gpu = st.selectbox( "GPU" , sorted(list(svb["gpu"])), index = "", placeholder ="GPU")
-opsys= st.selectbox( "Operating System" , sorted(list(svb["opsys"])), index = "", placeholder ="Operating System")
-weight= st.selectbox( "Weight" , sorted(list(svb["weight"])), index = "", placeholder ="Weight")
+company = st.selectbox( "Company" , [""] +sorted(list(svb["company"])),  placeholder = "Company")
+company = st.selectbox( "Company" , [""] +sorted(list(svb["company"])),  placeholder = "Company")
+typename = st.selectbox( "Type Name", [""] +sorted(list(svb["typename"])),  placeholder = "Type Name")
+inches = st.selectbox( "Inches", [""] +sorted(list(svb["inches"])), placeholder = "Inches")
+screenresolution =  st.selectbox( "Screen Resolution" , [""] +sorted(list(svb["screenresolution"])), placeholder = "Screen Resolution")
+cpu= st.selectbox( "CPU", [""] +sorted(list(svb["cpu"])), placeholder = "CPU")
+ram = st.selectbox( "RAM", [""] + sorted(list(svb["ram"])),placeholder = "RAM")
+memory = st.selectbox( "Memory", [""] + sorted(list(svb["memory"])), placeholder = "Memory",)
+gpu = st.selectbox( "GPU" , [""] + sorted(list(svb["gpu"])),  placeholder ="GPU")
+opsys= st.selectbox( "Operating System" , [""] + sorted(list(svb["opsys"])),  placeholder ="Operating System")
+weight= st.selectbox( "Weight" , [""] +  sorted(list(svb["weight"])), placeholder ="Weight")
 
 features = [company, product, typename, inches, screenresolution, cpu, ram, memory, gpu, opsys, weight ]  
 price= result(features)
