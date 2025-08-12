0f88bdc7170a5d19ffe0813c7037dd079fd52d10
Right Fix <righteousnessude@gmail.com>
2025-08-12 20:20:31 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ for keys in svb.keys():
 features = None #[company, product, typename, inches, screenresolution, cpu, ram, memory, gpu, opsys, weight ]  
 price= None #result(features)
 if st.button("Click"):
-  st.write(f"Your Budget should be £{round(price[0],-2):,}")
+  #st.write(f"Your Budget should be £{round(price[0],-2):,}")
   st.write("Your specification are :- ")
   features_name = [x for x in svb.keys()]
   for f, fn in zip(features, features_name):
