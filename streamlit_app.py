ae444f6ba5d637ef017d086cfb73a239253ef3de
Right Fix <righteousnessude@gmail.com>
2025-08-12 18:52:55 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,19 +6,7 @@ st.title("Laptop Price Model")
 st.write(
     "Select your laptop specification so you would know your budget"
 )
-"""
-company = st.selectbox( "Company" , sorted(list(svb["company"])),  placeholder = "Company")
-product= st.selectbox( "Product" , sorted(list(svb["product"])),  placeholder = "Product")
-typename = st.selectbox( "Type Name",  sorted(list(svb["typename"])),  placeholder = "Type Name")
-inches = st.selectbox( "Inches", sorted(list(svb["inches"])), placeholder = "Inches")
-screenresolution =  st.selectbox( "Screen Resolution" ,  sorted(list(svb["screenresolution"])), placeholder = "Screen Resolution")
-cpu= st.selectbox( "CPU", sorted(list(svb["cpu"])), placeholder = "CPU")
-ram = st.selectbox( "RAM", sorted(list(svb["ram"])),placeholder = "RAM")
-memory = st.selectbox( "Memory", sorted(list(svb["memory"])), placeholder = "Memory",)
-gpu = st.selectbox( "GPU" , sorted(list(svb["gpu"])),  placeholder ="GPU")
-opsys= st.selectbox( "Operating System" ,  sorted(list(svb["opsys"])),  placeholder ="Operating System")
-weight= st.selectbox( "Weight" , sorted(list(svb["weight"])), placeholder ="Weight")
-"""
+
 
 for keys in svb.keys():
   def declare_var():
