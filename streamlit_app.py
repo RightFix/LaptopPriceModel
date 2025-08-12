f7881d6217d380be7a7dd7744cf10588abbcdd9c
Right Fix <righteousnessude@gmail.com>
2025-08-12 20:33:27 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,16 @@ st.write(
 )
 
 for keys in svb.keys():
-  #def declare_var():
-    #globals()[keys] = 
-    st.selectbox( keys , sorted(list(svb[keys])), placeholder = keys)
- # declare_var()
+  def declare_var():
+    globals()[keys] = st.selectbox( keys , sorted(list(svb[keys])), placeholder = keys)
+  declare_var()
 
-features = None #[company, product, typename, inches, screenresolution, cpu, ram, memory, gpu, opsys, weight ]  
-price= None #result(features)
+features = [company, product, typename, inches, screenresolution, cpu, ram, memory, gpu, opsys, weight ]  
+price= result(features)
 if st.button("Click"):
-  #st.write(f"Your Budget should be £{round(price[0],-2):,}")
+  st.write(f"Your Budget should be £{round(price[0],-2):,}")
   st.write("Your specification are :- ")
   features_name = [x for x in svb.keys()]
-  #for f, fn in zip(features, features_name):
-        # st.write(f" {fn}: {f}")
+  for f, fn in zip(features, features_name):
+        st.write(f" {fn}: {f}")
 
