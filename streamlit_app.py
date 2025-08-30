6f66a5400d0ccb3c2e2de963ab87c6a46ebe6eb9
Right Fix <righteousnessude@gmail.com>
2025-08-30 21:03:26 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,14 @@ for keys in svb.keys():
 price= result(features)
 
 if st.button("Click"):
-  if " " in features or 0 in features:
-    st.write("Incomplete input")
+    
+     if " " in features or 0 in features:
+        st.write("Incomplete input")
   
-  else:
-    st.write(f"Your Budget should be £{round(price[0],-2):,}")
-    st.write("Your specification are :- ")
-    features_name = [x for x in svb.keys()]
-    for f, fn in zip(features, features_name):
-        st.write(f" {fn}: {f}")
+     else:
+        st.write(f"Your Budget should be £{round(price[0],-2):,}")
+        st.write("Your specification are :- ")
+        features_name = [x for x in svb.keys()]
+        for f, fn in zip(features, features_name):
+            st.write(f" {fn}: {f}")
 
