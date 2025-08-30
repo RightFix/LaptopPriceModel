8412915462324544be40433290243352160fd755
Right Fix <righteousnessude@gmail.com>
2025-08-30 20:03:11 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write(
 )
 
 features = []
-svb.pop("Weight")
+we = svb.pop("Weight")
 for keys in svb.keys():
   globals()[keys] = st.selectbox( keys.upper() , [" "] + sorted(list(svb[keys])), index = 0)
   features.append(globals()[keys])
