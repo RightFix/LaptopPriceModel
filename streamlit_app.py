8c94de70bd2f404a0adccddfd9d60880e4669b26
Right Fix <righteousnessude@gmail.com>
2025-09-01 01:10:28 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,15 @@ st.write(
     "Select your laptop specification so you would know your budget"
 )
 
+# Creation of selectioon  box and cooolecctionn  of user input
 features = []
 for keys in svb.keys():
     
     globals()[keys] = st.selectbox( keys.upper() ,
         [" "] +  sorted(list(svb[keys])) if keys != "inches" else [0] + sorted(list(svb[keys])))
     features.append(globals()[keys])
-    
+
+#Sends user innput to  the result function in  the main.py 
 price= result(features)
 
 filled_box = [x for x in features if x != "" or x != 0]
